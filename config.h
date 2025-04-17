9899d81d83b489d7a7e3e01c3ac5fb1acb3ac673
Sofronio <xbox2xbox@gmail.com>
2025-04-17 19:33:04 +0800

    v2.6.7

--- config.h
+++ config.h
@@ -26,7 +26,7 @@
 //#define CHECKBATTERY
 
 //SCALE CONFIG
-#define LINE1 (char*)"FW: 2.6.6"
+#define LINE1 (char*)"FW: 2.6.7"
 #define LINE2 (char*)"Built-date(YYYYMMDD): 20250417"
 #define LINE3 (char*)"S/N: HDS001"  //Serial number
 #define VERSION /*version*/ LINE1, /*compile date*/ LINE2, /*sn*/ LINE3
