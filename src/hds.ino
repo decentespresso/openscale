eede5c7665356efec2981b414959bc913d055a4b
Sofronio <xbox2xbox@gmail.com>
2026-01-28 11:26:20 +0800

    Update hds.ino

--- src/hds.ino
+++ src/hds.ino
@@ -1341,7 +1341,7 @@ void loop() {
       t_heartBeat = millis() + 10000; //only disconnect after 10 seconds, avoid frequent disconnecting.
     } 
   }
-  if (millis() - t_heartBeat < 1000)
+  if (millis() - t_heartBeat < 500)
     b_heartBeatIcon = true;
   else
     b_heartBeatIcon = false;
